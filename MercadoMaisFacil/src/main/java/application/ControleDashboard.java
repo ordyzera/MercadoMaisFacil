07d6d4bfaf64dca73822caaa330835e5a4a59844
pedro <pedro@192.168.100.26>
2026-09-22 17:21:35 -0300

    Atualização do dashboard

--- MercadoMaisFacil/src/main/java/application/ControleDashboard.java
+++ MercadoMaisFacil/src/main/java/application/ControleDashboard.java
@@ -1,10 +1,13 @@
 package application;
 
+import java.awt.Label;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 public class ControleDashboard {
 
@@ -46,8 +49,21 @@ public class ControleDashboard {
 
     @FXML
     private ImageView imgLogoMercadoMaisFacil;
+    
+    @FXML
+    private Label lblVendasHoje;
+    
+    @FXML
+    private AnchorPane painelDashboard;
 
     @FXML
     private TextField txtPesquisa;
-
+    
+    
+    @FXML
+    public void initialize() {
+        lblVendasHoje.setText("R$ 1.254,80");
+    }
 }
+
+
